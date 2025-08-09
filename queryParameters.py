bb8a5382e52d128487d19095836167e3c3a975d2
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-09 23:53:49 +0300

    Optional Parameters

--- queryParameters.py
+++ queryParameters.py
@@ -7,4 +7,4 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/query/")
 async def queryParam(skip: int=0, limit: int=10):
     return fake_items_db[skip: skip+limit]
-    
+
