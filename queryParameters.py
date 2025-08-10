9e6a5d1cc4e742c8a66ea1595d0dff4a4948cc22
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-10 07:56:56 +0300

    Optional Parameters

--- queryParameters.py
+++ queryParameters.py
@@ -8,3 +8,12 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 async def queryParam(skip: int=0, limit: int=10):
     return fake_items_db[skip: skip+limit]
 
+#Optional Parameters
+from typing import Optional
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id":item_id, "q":q}
+    return {"item_id":item_id}
+
