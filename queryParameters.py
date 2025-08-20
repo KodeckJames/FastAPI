4c6ecef548b2cf073d9a88851e69613c5eaee7e2
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-21 01:07:10 +0300

    Dynamic query parameters

--- queryParameters.py
+++ queryParameters.py
@@ -42,3 +42,11 @@ async def get_user_item(user_id: int, item_id: str, q: str | None = None, r: boo
 async def get_needy(item_id: str, needy: str):
     items=[{"item_id":item_id},{"needy":needy}]
     return items
+
+#And of course, you can define some parameters as required, some as having a default value, and some entirely optional:
+@app.get("/itemuz/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
