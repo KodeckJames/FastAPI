ee49c11f6d618739b9465ead54dcd82bfee7311d
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-20 20:13:29 +0300

    Query Parameter type conversion

--- queryParameters.py
+++ queryParameters.py
@@ -18,11 +18,11 @@ async def get_item(item_id: str, q: Optional[str] = None):
     return {"item_id":item_id}
 
 #Query Parameter type conversion
-@app.get("/param_type/{item_idd}")
-async def param_type_conv(item_idd : str, q : str | None = None, s : bool = False):
-    items_arr = {"item_idd":item_idd}
+@app.get("/itemz/{item_id}")
+async def read_item(item_id: str, q: str | None = None, r: bool = False):
+    items = {"Item_id": item_id}
     if q:
-        items_arr.update({"q":q})
-    if not s:
-        items_arr.update({"Yellow": "Yellow Yellow"})
-    return items_arr
+        items.update({"q":q})
+    if not r:
+        items.update({"Test":"This is the test Query Parameter type conversion"})    
+    return items
