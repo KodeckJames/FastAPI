730fa32def4f1809e9d9099bddc10cba6521926b
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-25 06:25:13 +0300

    Excluding Parameters from OpenAPI

--- queryParamsandstringValidations.py
+++ queryParamsandstringValidations.py
@@ -117,3 +117,11 @@ async def read_items(
     if q:
         results.update({"q": q})
     return results
+
+#Excluding Parameters from OpenAPI
+@app.get("/exclude/")
+async def get_item(hidden_query:Annotated[str|None, Query(include_in_schema=False)]=None):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    else:
+        return {"hidden_query": "Not found"}
