71e72a8939618dda280f21c2e701ce3fbd20ff7d
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-26 11:23:27 +0300

    Number validations: floats, greater than and less than

--- pathParamsandnumericvalidations.py
+++ pathParamsandnumericvalidations.py
@@ -11,7 +11,7 @@ async def read_item(item_id:Annotated[int, Path(title="This is the title of the 
     return results
 
 #Number validations: greater than or equal
-@app.get("/numvalidation/{item_id}")
+@app.get("/numvalidations/{item_id}")
 async def read_item(item_id:Annotated[int, Path(title="Item ID", ge=10)], q:str):
     results={"item_id":item_id}
     if q:
@@ -20,8 +20,18 @@ async def read_item(item_id:Annotated[int, Path(title="Item ID", ge=10)], q:str)
 
 #Number validations: greater than and less than or equal
 @app.get("/numvalidations2/{item_id}")
-async def read_item(item_id:Annotated[int, Path(gt=0, le=1000)], q: str):
+async def read_item(*,item_id:Annotated[int, Path(gt=0, le=1000)], q: str):
     results={"item_id":item_id}
     if q:
         results.update({"q":q})
+    return results
+
+#Number validations: floats, greater than and less than
+@app.get("/floatval/{item_id}")
+async def read_item(item_id:Annotated[int, Path(title="Item Id")], q:str, size:Annotated[float, Query(gt=0, lt=10.5)]):
+    results={"Item_id":item_id}
+    if q:
+        results.update({"q":q})
+    if size:
+        results.update({"size":size})
     return results
