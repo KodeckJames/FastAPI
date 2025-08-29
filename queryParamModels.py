2b3cf332977846f3b225c2a2fbdaa68b936707b5
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-29 13:01:12 +0300

    Forbid Extra Query Parameters

--- queryParamModels.py
+++ queryParamModels.py
@@ -6,10 +6,13 @@ app=FastAPI()
 
 class Model_defined(BaseModel):
     score:int=Field(100, gt=1, le=1000)
-    marks:int=Field(0, gt=10)
+    marks:int=Field(20, gt=10)
     names:Literal['JJ','Ondix']="JJ"
     people:list[str]=[]
 
+    #Forbid Extra Query Parameters
+    model_config={"extra":"forbid"}
+
 @app.get("/items/")
-async def get_items(defined:Annotated[Model_defined, Query()]):
-    return defined
+async def get_items(filtered:Annotated[Model_defined, Query()]):
+    return filtered
