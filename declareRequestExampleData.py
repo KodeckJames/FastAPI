80d9a57ed8f1f2f534e7929b5150c3ea7e09f05c
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-12 00:11:02 +0300

    Body with multiple examples

--- declareRequestExampleData.py
+++ declareRequestExampleData.py
@@ -26,10 +26,18 @@ class Item(BaseModel):
 
 @app.put("/items/{item_id}")
 async def put_item(item_id:int, item:Annotated[Item, Body(examples=[{
-    "name": "Foo",
-    "description": "A very nice Item",
-    "price": 35.4,
-    "tax": 3.2,
-}])]):
+        "name": "Foo",
+        "description": "A very nice Item",
+        "price": 35.4,
+        "tax": 3.2,
+    },
+    {
+        "name": "Bar",
+        "price": "35.4",
+    },
+    {
+        "name": "Baz",
+        "price": "thirty five point four",
+    },])]):
     result={"item_id":item_id, "item":item}
     return result
