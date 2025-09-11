c64ee9b70d8ddf6e1d80c5ce0f35bca882af10e5
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-11 09:27:27 +0300

    Embedding a single body parameter

--- bodyMultipleParams.py
+++ bodyMultipleParams.py
@@ -16,6 +16,7 @@ class User(BaseModel):
 
 @app.put("/items/{item_id}")
 async def put_item(
+    *,
     item_id:Annotated[int, Path(description="Hello, World!", ge=0, le=1000 )],
     importance:Annotated[int, Body(alias="import")],
     q:Annotated[str|None, Query(alias="qparam")]=None,
@@ -32,3 +33,36 @@ async def put_item(
     if importance:
         result.update({"importance":importance})
     return result
+
+#Embed a single body parameter
+#Let's say you only have a single item body parameter from a Pydantic model Item.
+# By default, FastAPI will then expect its body directly.
+# But if you want it to expect a JSON with a key item and inside of it the model contents, as it does when you declare extra body parameters, you can use the special Body parameter embed:
+class Item_lite(BaseModel):
+    name: str
+    description:str|None=None
+    value:float
+    tax:float|None=None
+
+@app.put("/itemz")
+async def add_item(item:Annotated[Item_lite, Body(embed=True)]):
+    result={"item":item}
+    return result
+
+# In this case FastAPI will expect a body like:
+{
+    "item": {
+        "name": "Foo",
+        "description": "The pretender",
+        "price": 42.0,
+        "tax": 3.2
+    }
+}
+
+# instead of:
+{
+    "name": "Foo",
+    "description": "The pretender",
+    "price": 42.0,
+    "tax": 3.2
+}
