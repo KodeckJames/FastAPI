3206126d649e586f7953e387a726007c1efd911e
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-11 17:08:46 +0300

    List Fields

---
+++ bodyNestedModels.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+class Item(BaseModel):
+    name:str
+    description:str|None=None
+    price:float
+    tax:float|None=None
+    tags:list=[]
+
+@app.put("/item/{item_id}")
+async def put_item(item_id:int, item:Item):
+    result={"item_id":item_id, "item":item}
+    return result
