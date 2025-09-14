6f770968308b424f599a1c05c43734bbb4daef95
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-14 16:06:21 +0300

    Setting convert_underscores param to True

--- headerParamModels.py
+++ headerParamModels.py
@@ -5,6 +5,8 @@ from typing import Annotated
 app=FastAPI()
 
 class CommonHeaders(BaseModel):
+    model_config={"extra":"forbid"}
+
     host:str
     save_data:bool
     if_modified_since:str|None=None
@@ -12,5 +14,5 @@ class CommonHeaders(BaseModel):
     x_tag:list[str]=[]
     
 @app.get("/items/")
-async def get_items(headers:Annotated[CommonHeaders, Header()]):
+async def get_items(headers:Annotated[CommonHeaders, Header(convert_underscores=True)]):
     return headers
