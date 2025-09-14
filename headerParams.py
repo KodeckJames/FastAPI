470559bd4ccff9748cd524eae4015e82f7d0e00a
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-14 09:39:22 +0300

    Duplicate headers

--- headerParams.py
+++ headerParams.py
@@ -4,6 +4,22 @@ from typing import Annotated
 app=FastAPI()
 
 @app.get("/items/")
-async def get_items(header:Annotated[str, Header(convert_underscores=False)]):
+async def get_items(header:Annotated[list[str]|None, Header(convert_underscores=False)]=None):
     result={"header":header}
     return result
+
+#Duplicate headers
+# It is possible to receive duplicate headers. That means, the same header with multiple values.
+# If you communicate with that path operation sending two HTTP headers like:
+
+# header: foo
+# header: bar
+
+# The response would be like:
+
+# {
+#     "header values": [
+#         "bar",
+#         "foo"
+#     ]
+# }
