30651bd7128148e6f7bcf461807f7c963bd7b900
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-22 19:11:40 +0300

    Using fastapi.status as a shortcut to remember the status codes

--- responseStatusCode.py
+++ responseStatusCode.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi import FastAPI, status, Query
+from typing import Annotated
 
 app=FastAPI()
 
 @app.get("/items/", status_code=201)
 async def get_item(name:str):
     return {"name":name}
+
+# Shortcut to remember the status code names:
+# You can use the convenience variables from fastapi.status
+@app.get("/car/", status_code=status.HTTP_201_CREATED)
+async def create_car(carName:Annotated[str, Query()]="GLE"):
+    return {"Car_name":carName}
