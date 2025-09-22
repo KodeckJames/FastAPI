ce065bac61d3c5e9b3502a9ece388f5f87b74658
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-22 17:11:39 +0300

    Reducing duplication using class inheritance

--- extraModels.py
+++ extraModels.py
@@ -14,7 +14,7 @@ class UserOut(BaseModel):
     email:EmailStr
     full_name:str|None=None
 
-class DBIn(BaseModel):
+class UserInDB(BaseModel):
     username:str
     hashed_password:str
     email:EmailStr
@@ -25,7 +25,39 @@ def fake_password_hasher(raw_password:str):
 
 def fake_save_user(user_in:UserIn):
     hashed_password=fake_password_hasher(user_in.password)
-    user_in_db=DBIn(**user_in.model_dump(), hashed_password=hashed_password)
+    user_in_db=UserInDB(**user_in.model_dump(), hashed_password=hashed_password)
+    print("User saved!... not really")
+    return user_in_db
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in:UserIn):
+    user_saved=fake_save_user(user_in)
+    return user_saved
+
+# Reducing duplication using class inheritance
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+def fake_password_hasher(raw_password:str):
+    return (f"Super Secret: {raw_password}")
+
+def fake_save_user(user_in:UserIn):
+    hashed_password=fake_password_hasher(user_in.password)
+    user_in_db=UserInDB(**user_in.model_dump(), hashed_password=hashed_password)
     print("User saved!... not really")
     return user_in_db
 
