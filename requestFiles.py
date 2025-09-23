312d773ee21f1b3f4d366c8f33b2780ed24d28b7
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-23 18:46:24 +0300

    Multiple file uploads

--- requestFiles.py
+++ requestFiles.py
@@ -1,6 +1,7 @@
 # You can define files to be uploaded by the user using File
 from fastapi import FastAPI, File, UploadFile
 from typing import Annotated
+from fastapi.responses import HTMLResponse
 
 app=FastAPI()
 
@@ -25,3 +26,28 @@ async def optional2(myfileOptional:Annotated[UploadFile|None, File(description="
     if myfileOptional:
         return{"Name":myfileOptional.filename}
     return{"Message":"No file uploaded"}
+
+# Multiple file uploads
+@app.post("/optionalMultipleFile/")
+async def multiple_files(userListFiles:Annotated[list[bytes], File(description="This is a multiple file upload API")]):
+   return {"File_Length":[len(file) for file in userListFiles]}
+
+@app.post("/optionalMultipleUploadFile/")
+async def multiple_files2(myfileList:Annotated[list[UploadFile], File(description="This is a multiple file upload API")]):
+    return{"Name":[file.filename for file in myfileList]}
+   
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
