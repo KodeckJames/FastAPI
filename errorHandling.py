2bff539c896ea8d2a1b4b14628b67655661cbe56
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-25 01:13:51 +0300

    Raising errors with HTTPException

---
+++ errorHandling.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, HTTPException, Path
+from typing import Annotated
+
+app=FastAPI()
+
+items={
+    "car":"This is a Mercedes SUV called the GLE"
+}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id:Annotated[str, Path()]):
+    if item_id not in items:
+        raise HTTPException(detail="This item doesn't exist", status_code=404)
+    return items[item_id]
