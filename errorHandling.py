9e85740cf6ee073a95fad8d245b243821296b3b2
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-25 13:58:31 +0300

    Custom Exception handlers �

--- errorHandling.py
+++ errorHandling.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException, Path
+from fastapi import FastAPI, HTTPException, Path, Request
 from typing import Annotated
+from fastapi.responses import JSONResponse
 
 app=FastAPI()
 
@@ -12,3 +13,21 @@ async def get_item(item_id:Annotated[str, Path()]):
     if item_id not in items:
         raise HTTPException(detail={"Error":"This item doesn't exist"}, status_code=404, headers={"X-Error":"This is a not found error"})
     return items[item_id]
+
+# Custom Exception handlers
+class UnicornException(Exception):
+    def __init__(self, name:str):
+        self.name=name
+
+@app.exception_handler(UnicornException)
+async def custom_exception(request:Request, exc:UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"Message":f"Oops! {exc.name} is not available"}
+    )
+
+@app.get("/items2/{name}")
+async def raise_exception(name:str):
+    if name=="yellow":
+        raise UnicornException(name=name)
+    return {"name":name}
