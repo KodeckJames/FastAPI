6b2f360545bf91648773b94269684359fde4ad7d
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-25 16:50:27 +0300

    Override request validation exceptions

--- errorHandling.py
+++ errorHandling.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, Path, Request
 from typing import Annotated
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app=FastAPI()
 
@@ -26,8 +28,24 @@ async def custom_exception(request:Request, exc:UnicornException):
         content={"Message":f"Oops! {exc.name} is not available"}
     )
 
-@app.get("/items2/{name}")
-async def raise_exception(name:str):
-    if name=="yellow":
-        raise UnicornException(name=name)
-    return {"name":name}
+@app.get("/items2/{name_passed}")
+async def raise_exception(name_passed:str):
+    if name_passed=="yellow":
+        raise UnicornException(name=name_passed)
+    return {"name":name_passed}
+
+# Override the default exception handlers
+# Override request validation exceptions
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+@app.get("/items3/{item_id}")
+async def get_item(item_id:int):
+    if item_id==3:
+        raise HTTPException(status_code=418, detail="Nope, 3 is not the number")
+    return{"Item_id":item_id}
