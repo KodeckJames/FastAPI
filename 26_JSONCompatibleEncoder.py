3d304bad2d43330adb09027535b8653f8af44fce
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-09-29 13:00:38 +0300

    JSON compatible Encoder

---
+++ 26_JSONCompatibleEncoder.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from datetime import datetime
+
+app=FastAPI()
+
+fake_DB={}
+
+class Item(BaseModel):
+    name:str
+    description:str
+    date:datetime
+
+@app.put("/items/{id}")
+async def update_item(item:Item, id:str):
+    jsonable_converted_data=jsonable_encoder(item)
+    fake_DB[id]=jsonable_converted_data
