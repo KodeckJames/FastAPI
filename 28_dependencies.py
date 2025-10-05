351320eb98a9ed9973f4860d5d96f09cdf2ba088
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-10-05 16:15:47 +0300

    Dependency injection

---
+++ 28_dependencies.py
@@ -0,0 +1,17 @@
+# Creating a dependency/ dependable
+# A dependency is just a function that can take all the parameters that a path operation function can take
+from fastapi import FastAPI, Depends
+from typing import Annotated
+
+app=FastAPI()
+
+async def common_params(q:str|None=None, skip:int=0, limit:int=100):
+    return{"q":q, "skip":skip, "limit":limit}
+
+@app.get("/items")
+async def get_items(commons:Annotated[dict, Depends(common_params)]):
+    return commons
+
+@app.get("/users")
+async def get_users(user_commons:Annotated[dict, Depends(common_params)]):
+    return user_commons
