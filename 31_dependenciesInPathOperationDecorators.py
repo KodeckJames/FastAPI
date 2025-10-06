00c33d8a94d0c23a9b3cf924c8786651102f0a0d
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-10-06 14:25:39 +0300

    Dependencies passed in Path Operation Decorators

---
+++ 31_dependenciesInPathOperationDecorators.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Depends, HTTPException, Header,status
+from typing import Annotated
+
+app=FastAPI()
+
+async def verify_token(token:Annotated[str, Header()]):
+    if token != "Fake-super-secret-token":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Token Header invalid")
+    
+async def verify_key(tokenKey:Annotated[str, Header()]):
+    if tokenKey != "Fake-secret-super-key":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Token key is invalid")
+    
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def get_item():
+    return [{"item": "Foo"}, {"item": "Bar"}]
