26def60c9c9169537577716352da792f2c4c65bc
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-10-06 16:31:01 +0300

    Global Dependencies

---
+++ 32_globalDependencies.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Header, HTTPException, Depends, dependencies, status
+from typing import Annotated
+
+async def verify_token(q:Annotated[str|None, Header()]=None):
+    if q!="secret-encrypted-token":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token!")
+    
+async def verify_key(q:Annotated[str|None, Header()]=None):
+    if q!="secret-encoded-key":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid Key!")
+    return {"q":q}
+
+app=FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+@app.get("/items/")
+async def get_item():
+    return [{"name":"Yellow"},{"place":"The Piano Hub"},{"Food":"Pizza"}]
+
+@app.get("/cars/")
+async def get_car():
+    return [{"Brand":"Mercedes"},{"Type":"GLE"},{"Price":10000000}]
