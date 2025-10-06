25018e37f16c7c4c17110657514daf5fd9a869f7
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-10-06 23:55:56 +0300

    Always raise in Dependencies with yield and except

--- 33_dependenciesWithYield.py
+++ 33_dependenciesWithYield.py
@@ -99,3 +99,33 @@ def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
             status_code=404, detail="Item not found, there's only a plumbus here"
         )
     return item_id
+
+# Always raise in Dependencies with yield and except
+# If you catch an exception in a dependency with yield, unless you are raising another HTTPException or similar, you should re-raise the original exception.
+
+class InternalError(Exception):
+    pass
+
+class InternalError(Exception):
+    pass
+
+
+def get_username():
+    try:
+        yield "Rick"
+    except InternalError:
+        print("We don't swallow the internal error here, we raise again 😎")
+        raise
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
+    if item_id == "portal-gun":
+        raise InternalError(
+            f"The portal gun is too dangerous to be owned by {username}"
+        )
+    if item_id != "plumbus":
+        raise HTTPException(
+            status_code=404, detail="Item not found, there's only a plumbus here"
+        )
+    return item_id
