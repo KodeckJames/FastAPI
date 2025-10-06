9976dae94a98f0c14f845470bc903f35076e1ffe
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-10-06 23:35:10 +0300

    Dependencies with yield and HTTPException

--- 33_dependenciesWithYield.py
+++ 33_dependenciesWithYield.py
@@ -6,8 +6,10 @@
 # Only the code prior to and including the yield statement is executed before creating a response:
 
 # The yielded value is what is injected into path operations and other dependencies:
-from fastapi import Depends
+from fastapi import Depends, FastAPI, HTTPException
 from typing import Annotated
+
+app=FastAPI()
 
 def DBSession():
     pass
@@ -50,6 +52,28 @@ async def dependency_c(dep_b:Annotated[str, Depends(dependency_b)]):
     finally:
         dep_c.close(dep_b)
 
+# Dependencies with yield and HTTPException
+data = {
+    "plumbus": {"description": "Freshly pickled plumbus", "owner": "Morty"},
+    "portal-gun": {"description": "Gun to create portals", "owner": "Rick"},
+}
+
+class OwnerError(Exception):
+    pass
+
+def get_username():
+    try:
+        yield "Rick"
+    except OwnerError as e:
+        raise HTTPException(status_code=400, detail=f"Owner error: {e}")
 
 
-    
+@app.get("/items/{item_id}")
+def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
+    if item_id not in data:
+        raise HTTPException(status_code=404, detail="Item not found")
+    item = data[item_id]
+    if item["owner"] != username:
+        raise OwnerError(username)
+    return item
+
