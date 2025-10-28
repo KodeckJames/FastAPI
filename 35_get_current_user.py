80b636c8e4fa567b1d26d9fd75c949f3eed489cc
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-10-28 18:39:35 +0300

    Getting the current user

---
+++ 35_get_current_user.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer
+
+app=FastAPI()
+
+oauth2_token=OAuth2PasswordBearer(tokenUrl="token")
+
+class User(BaseModel):
+    username:str
+    email:str|None=None
+    full_name:str|None=None
+    disabled:bool|None=None
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@eample.com", full_name="John Doe"
+    )
+
+async def get_current_user(token:Annotated[str, Depends(oauth2_token)]):
+    user = fake_decode_token(token)
+    return user
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
